e3c6562a96af64945e397e5fcaabd92bf7d97148
Nhat19990 <thaithanhnhat59@gmail.com>
2024-06-24 19:30:58 +0700

    them tinh nang

--- cpp_boiso.cpp
+++ cpp_boiso.cpp
@@ -1,30 +1,22 @@
-// CPP_LuyThua.cpp : This file contains the 'main' function. Program execution begins and ends there.
+
+// Boi_So.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
 #include <iostream>
 using namespace std;
 int main()
 {
-	cout << "TINH LUY THUA\n";
-	int so, somu, luythua = 1;
-	cout << "Nhap so";
+	int so, n, boiso;
+	cout << "Nhap so dau";
 	cin >> so;
-	cout << "Nhap mu";
-	cin >> somu;
-	for (int i = 0; i < somu; i++)
-	{
-		luythua *= so;
+	cout << "Nhap so cuoi";
+	cin >> n;
+	int i = 1;
+	boiso = so;
+	while (boiso < n) {
+		boiso = so * i;
+		i++;
+		cout << "Boi la" << boiso << " " << endl;
 	}
-	cout << "Luythua:" << luythua;
-
 	return 0;
 }
-
-/*cout << "Luythua = " << pow(so, somu) << endl;*/
-/*double tinhluythua(double a, double b) {
-	double giatri = 1;
-	for (int i = 1;i < b, i++) {
-		giatri = giatri * a;
-	}
-	return giatri
-}*/
