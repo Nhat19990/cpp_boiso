22590c59a08dfc1450744c6bc9f60f887917ad9f
Nhat19990 <thaithanhnhat59@gmail.com>
2024-06-24 19:28:53 +0700

    them

--- cpp_boiso.cpp
+++ cpp_boiso.cpp
@@ -1,30 +1,30 @@
-// cpp_boiso.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// CPP_LuyThua.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
 #include <iostream>
 using namespace std;
+int main()
+{
+	cout << "TINH LUY THUA\n";
+	int so, somu, luythua = 1;
+	cout << "Nhap so";
+	cin >> so;
+	cout << "Nhap mu";
+	cin >> somu;
+	for (int i = 0; i < somu; i++)
+	{
+		luythua *= so;
+	}
+	cout << "Luythua:" << luythua;
 
-int main() {
-    int n;
-    cout << "Nhap so n: ";
-    cin >> n;
-    cout << "Tat ca cac uoc cua so " << n << " la ";
-    for (int i = 1; i <= n; i++) {
-        if (n % i == 0) {
-            cout << i << " ";
-        }
-    }
-    return 0;
+	return 0;
 }
 
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+/*cout << "Luythua = " << pow(so, somu) << endl;*/
+/*double tinhluythua(double a, double b) {
+	double giatri = 1;
+	for (int i = 1;i < b, i++) {
+		giatri = giatri * a;
+	}
+	return giatri
+}*/
